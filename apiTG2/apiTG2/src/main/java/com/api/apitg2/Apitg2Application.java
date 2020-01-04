2da885d457227e0c2160c4dba51e905903493c6e
Carlos <carlosarak@gmail.com>
2020-01-04 15:02:45 -0300

    feat:Config api

---
+++ apiTG2/apiTG2/src/main/java/com/api/apitg2/Apitg2Application.java
@@ -0,0 +1,13 @@
+package com.api.apitg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apitg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apitg2Application.class, args);
+	}
+
+}
