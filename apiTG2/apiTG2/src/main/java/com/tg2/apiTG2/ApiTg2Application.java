cd772d314d599706489a7a3befaa46a17c8d0b89
Carlos <carlosarak@gmail.com>
2020-01-04 02:42:35 -0300

    feat: set Api Java workspace
    Configurado o Gradle para framework spring e arquivos/diretorios básicos para iniciar projeto da api

---
+++ apiTG2/apiTG2/src/main/java/com/tg2/apiTG2/ApiTg2Application.java
@@ -0,0 +1,13 @@
+package com.tg2.apiTG2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTg2Application.class, args);
+	}
+
+}
